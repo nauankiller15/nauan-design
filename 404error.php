6f44b57d73954a40f442217adccaff3ef91341be
nauankiller15 <nauan.alcantara1@gmail.com>
2020-09-28 18:20:31 -0300

    página de error 404

---
+++ 404error.php
@@ -0,0 +1,43 @@
+<?php include("includes/head404.php"); ?>
+<div class="LogoMenu">
+    <h2 class="nauan-sub">Nauan</h2>
+    <h2 class="design-sub">Design</h2>
+</div>
+
+<body>
+    <div id="slider">
+        <figure>
+            <img src="imgsBackground/1.jpg">
+            <img src="imgsBackground/2.jpg">
+            <img src="imgsBackground/3.jpg">
+            <img src="imgsBackground/4.jpg">
+            <img src="imgsBackground/5.jpg">
+            <img src="imgsBackground/1.jpg">
+            <img src="imgsBackground/3.jpg">
+            <img src="imgsBackground/4.jpg">
+            <img src="imgsBackground/5.jpg">
+            <img src="imgsBackground/1.jpg">
+        </figure>
+    </div>
+    <div id="esconder">
+        <div id="quadrado-position">
+            <div class="divSlide">
+                <h2 class="esta">Está</h2>
+                <h2 class="perdido">Perdido?</h2>
+                <div class="euSou">
+                <h1 style="animation: euSou 2.3s ease-in-out;"><span class="typed"></span></h1>
+                <p style="animation: animSou 1.3s ease-in-out;">Venha desenvolver junto comigo.</p>
+                </div>
+            </div>
+            <div class="quadrado-index">
+            </div>
+            <div class="quadrado404error">
+                <a href="index.php"><li style="animation: go-back1 3.5s infinite alternate, LiAnim 2.5s;" class="Voltar">Clique aqui pra voltar</li></a>
+            </div>
+        </div>
+    </div>
+
+    <?php include("includes/footer.php"); ?>
+</body>
+
+</html>
---
+++ includes/head404.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="imagens/favicon.ico" type="image/ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="CSSs/styleindex.css">
+    <link rel="stylesheet" type="text/css" href="CSSs/menu.css">
+    <link rel="stylesheet" type="text/css" href="CSSs/slide.css">
+    <link rel="stylesheet" type="text/css" href="CSSs/style404.css">
+    <title>Nauan - Design</title>
+
+</head>
