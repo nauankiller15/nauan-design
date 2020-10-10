a6c4d5d15ccc067e17b9da4efa83176cf1e304fc
nauankiller15 <nauan.alcantara1@gmail.com>
2020-10-10 17:51:00 -0300

    ativando responsivo completo

--- includes/menu.php
+++ includes/menu.php
@@ -15,7 +15,7 @@
 <div id="esconder-menu">
     <div class="menu">
         <ul class="dropdown-menu">
-            <li id="fechar" href="javascript: FecharTudo();">Fechar Tudo</li>
+            <li id="fechar" href="javascript: FecharTudo();"><i class="fas fa-home"></i> Voltar Inicio</li>
             <li id="trabalhosAbrir" style="animation: fadeIn 12.0s, slideMenu 2.2s;">Projetos</li>
             <li id="contatoAbrir" style="animation: fadeIn 12.0s, slideMenu 3.2s;">Contato</li>
             <li id="sobreAbrir" style="animation: fadeIn 12.1s, slideMenu 4.2s;">Experiências</li>
