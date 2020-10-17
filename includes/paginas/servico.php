19dc07ddbdb64f465e0539c94bff59b83ac5a170
nauankiller15 <nauan.alcantara1@gmail.com>
2020-10-17 13:19:22 -0300

    alterações visuais para melhor desenvolvimento

--- includes/paginas/servico.php
+++ includes/paginas/servico.php
@@ -1,17 +1,18 @@
 <div id="servico">
     <div class="servico">
-        <h2 style="animation: fadeIn 3.5s ease-in-out, flip-2-hor-top 1.8s cubic-bezier(0.455, 0.030, 0.515, 0.955) alternate-reverse;">SOLICITAR SERVIÇO</h2>
+        <h2 style="animation: fadeIn 3.5s ease-in-out, flip-2-hor-top 2.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) alternate-reverse;">SOLICITAR SERVIÇO</h2>
     </div>
     <div id="caixaServico">
         <div class="CentroServico">
-            <div class="separarCima">Escolha uma das opções abaixo para solicitar um atendimento:</div>
-            <div class="separarT">Ou você pode ir na tela de contato e solicitar também Contato via E-mail.</div>
+            <div style="animation: fadeIn 3.0s ease-in-out, flip-2-hor-top 3.0s cubic-bezier(0.455, 0.030, 0.515, 0.955) alternate-reverse;" class="separarCima">Escolha uma das opções abaixo para solicitar um atendimento:</div>
+            <div style="animation: go-back1 3.5s infinite alternate, fadeIn 3.0s ease-in-out, flip-2-hor-top 3.4s cubic-bezier(0.455, 0.030, 0.515, 0.955) alternate-reverse;" class="separarT">Os preços podem conter variações, dependendo do conteúdo solicitado, faça um orçamento comigo.</div>
             <div class="call-buttons">
                 <div style="animation: flip 3.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) reverse backwards;" class="whatssapp-btn">
-                <i style="margin-right:8px;" class="fab fa-whatsapp"></i>Chamar Whatssapp</div>
+                    <i style="margin-right:8px;" class="fab fa-whatsapp"></i>Chamar Whatssapp</div>
                 <div style="animation: flip 4.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) reverse backwards;" class="insta-btn">
-                <i style="margin-right:8px;" class="fab fa-instagram"></i>Chamar Direct</div>
+                    <i style="margin-right:8px;" class="fab fa-instagram"></i>Chamar Direct</div>
             </div>
+            <div style="animation: go-back1 3.0s infinite alternate, fadeIn 3.0s ease-in-out, flip-2-hor-top 3.9s cubic-bezier(0.455, 0.030, 0.515, 0.955) alternate-reverse;" class="separarT">Ou você pode ir na tela de Contato e solicitar um atendimento via E-mail também.</div>
         </div>
     </div>
 </div>
