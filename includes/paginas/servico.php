675863ab8c2eee379d255908d4dcfefbdffeb208
nauankiller15 <nauan.alcantara1@gmail.com>
2020-10-17 12:41:13 -0300

    solicitar serviço criado

--- includes/paginas/servico.php
+++ includes/paginas/servico.php
@@ -6,6 +6,12 @@
         <div class="CentroServico">
             <div class="separarCima">Escolha uma das opções abaixo para solicitar um atendimento:</div>
             <div class="separarT">Ou você pode ir na tela de contato e solicitar também Contato via E-mail.</div>
+            <div class="call-buttons">
+                <div style="animation: flip 3.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) reverse backwards;" class="whatssapp-btn">
+                <i style="margin-right:8px;" class="fab fa-whatsapp"></i>Chamar Whatssapp</div>
+                <div style="animation: flip 4.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) reverse backwards;" class="insta-btn">
+                <i style="margin-right:8px;" class="fab fa-instagram"></i>Chamar Direct</div>
+            </div>
         </div>
     </div>
 </div>
