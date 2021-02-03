de545b1feca732dbb620558ed1ad942d57993031
nauankiller15 <nauan.alcantara1@gmail.com>
2021-02-02 22:43:48 -0300

    links na página de solicitar servico

--- includes/paginas/servico.php
+++ includes/paginas/servico.php
@@ -7,10 +7,16 @@
             <div style="animation: fadeIn 3.0s ease-in-out, flip-2-hor-top 3.0s cubic-bezier(0.455, 0.030, 0.515, 0.955) alternate-reverse;" class="separarCima">Escolha uma das opções abaixo para solicitar um atendimento:</div>
             <div style="animation: go-back1 3.5s infinite alternate, fadeIn 3.0s ease-in-out, flip-2-hor-top 3.4s cubic-bezier(0.455, 0.030, 0.515, 0.955) alternate-reverse;" class="separarT">Os preços podem conter variações, dependendo do conteúdo solicitado, faça um orçamento comigo.</div>
             <div class="call-buttons">
-                <div style="animation: flip 3.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) reverse backwards;" class="whatssapp-btn">
-                    <i style="margin-right:8px;" class="fab fa-whatsapp"></i>Chamar Whatssapp</div>
-                <div style="animation: flip 4.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) reverse backwards;" class="insta-btn">
-                    <i style="margin-right:8px;" class="fab fa-instagram"></i>Chamar Direct</div>
+                <a href="https://api.whatsapp.com/message/HQZ2ICED6X64G1" target="_blank">
+                    <div style="animation: flip 3.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) reverse backwards;" class="whatssapp-btn">
+                        <i style="margin-right:8px;" class="fab fa-whatsapp"></i>Chamar Whatssapp
+                    </div>
+                </a>
+                <a href="https://www.instagram.com/nauandesign/" target="_blank">
+                    <div style="animation: flip 4.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) reverse backwards;" class="insta-btn">
+                        <i style="margin-right:8px;" class="fab fa-instagram"></i>Chamar Direct
+                    </div>
+                </a>
             </div>
             <div style="animation: go-back1 3.0s infinite alternate, fadeIn 3.0s ease-in-out, flip-2-hor-top 3.9s cubic-bezier(0.455, 0.030, 0.515, 0.955) alternate-reverse;" class="separarT">Ou você pode ir na tela de Contato e solicitar um atendimento via E-mail também.</div>
         </div>
