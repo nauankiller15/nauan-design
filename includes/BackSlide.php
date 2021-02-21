c31f6af64ff624301c57e267fdba35cd68c7364a
nauankiller15 <nauan.alcantara1@gmail.com>
2021-02-20 23:39:00 -0300

    stories insta

--- includes/BackSlide.php
+++ includes/BackSlide.php
@@ -1,5 +1,5 @@
-<div class="fundo-prot">
-</div>
+<!-- <div class="fundo-prot">
+</div> -->
 <!-- <div id="slider">
     <figure>
         <img src="https://1.bp.blogspot.com/-hJgAap3oEfQ/X52RlfvjinI/AAAAAAAAGb0/sydDv1KfXHQ8IYZZdEIysxNzLkIzRVQqgCLcBGAsYHQ/s0/Background1.png">
@@ -14,19 +14,23 @@
         <img src="https://1.bp.blogspot.com/-hJgAap3oEfQ/X52RlfvjinI/AAAAAAAAGb0/sydDv1KfXHQ8IYZZdEIysxNzLkIzRVQqgCLcBGAsYHQ/s0/Background1.png">
     </figure>
 </div> -->
-
+<span class="message"><img src="https://1.bp.blogspot.com/-kxwlsl8gb_E/YAr4MtQDSMI/AAAAAAAAGjs/PjD4X1OIkjYFi-F_UpEG22ttIY6OOnM5QCLcBGAsYHQ/s0/Nauan-Design-logo-brand.png">
+   <b>Nauan-Design</b>
+    <i>backgrounds da página</i>
+</span>
 <div id="slider">
     <div data-slide="slide" class="slide">
         <div class="slide-items">
-        <img src="https://1.bp.blogspot.com/-hJgAap3oEfQ/X52RlfvjinI/AAAAAAAAGb0/sydDv1KfXHQ8IYZZdEIysxNzLkIzRVQqgCLcBGAsYHQ/s0/Background1.png">
-        <img src="https://1.bp.blogspot.com/-GiOVC7zXgn8/X52Se6stW_I/AAAAAAAAGb8/S31URfTE8Q8bwq-tXJZVvc-XXS1o2AGugCLcBGAsYHQ/s0/background2.png">
-        <img src="https://1.bp.blogspot.com/-HhKzDXo1kvQ/X52T80atZKI/AAAAAAAAGcI/l2aOarZPOpM9d_GxSqVc0unCxgpu01OJgCLcBGAsYHQ/s0/background3.png">
-        <img src="https://1.bp.blogspot.com/-npVdZ1nW-Sw/X52V5KYQnMI/AAAAAAAAGcU/lSD9NaKb0VQzl15NVZYnH_d9GOiAhpoFQCLcBGAsYHQ/s0/background4.png">
-        <img src="https://1.bp.blogspot.com/-Cnxey-K4W6w/X52YJBX7gII/AAAAAAAAGcg/Y3DveUnBN54YjWaLy1SO6eRsMvzfcEBQwCLcBGAsYHQ/s0/background5.png">
+            <img src="https://1.bp.blogspot.com/-hJgAap3oEfQ/X52RlfvjinI/AAAAAAAAGb0/sydDv1KfXHQ8IYZZdEIysxNzLkIzRVQqgCLcBGAsYHQ/s0/Background1.png">
+            <img src="https://1.bp.blogspot.com/-GiOVC7zXgn8/X52Se6stW_I/AAAAAAAAGb8/S31URfTE8Q8bwq-tXJZVvc-XXS1o2AGugCLcBGAsYHQ/s0/background2.png">
+            <img src="https://1.bp.blogspot.com/-HhKzDXo1kvQ/X52T80atZKI/AAAAAAAAGcI/l2aOarZPOpM9d_GxSqVc0unCxgpu01OJgCLcBGAsYHQ/s0/background3.png">
+            <img src="https://1.bp.blogspot.com/-npVdZ1nW-Sw/X52V5KYQnMI/AAAAAAAAGcU/lSD9NaKb0VQzl15NVZYnH_d9GOiAhpoFQCLcBGAsYHQ/s0/background4.png">
+            <img src="https://1.bp.blogspot.com/-Cnxey-K4W6w/X52YJBX7gII/AAAAAAAAGcg/Y3DveUnBN54YjWaLy1SO6eRsMvzfcEBQwCLcBGAsYHQ/s0/background5.png">
         </div>
         <nav class="slide-nav">
             <div class="slide-thumb"></div>
-            <button class="slide-prev">Prev</button>
-            <button class="slide-next">Next</button>
+            <button class="slide-prev">
+                <</button>
+                    <button class="slide-next">></button>
         </nav>
     </div>
