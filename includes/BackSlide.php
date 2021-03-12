4b0ce4087f197d7b2ac354b55ef6de4d0c472a46
nauankiller15 <nauan.alcantara1@gmail.com>
2021-03-12 15:12:36 -0300

    ajuste de background

--- includes/BackSlide.php
+++ includes/BackSlide.php
@@ -23,7 +23,7 @@
         <div class="slide-items">
             <div class="rotateSlides"><img src="https://1.bp.blogspot.com/-hJgAap3oEfQ/X52RlfvjinI/AAAAAAAAGb0/sydDv1KfXHQ8IYZZdEIysxNzLkIzRVQqgCLcBGAsYHQ/s0/Background1.png"></div>
             <div class="rotateSlides"><img src="https://1.bp.blogspot.com/-GiOVC7zXgn8/X52Se6stW_I/AAAAAAAAGb8/S31URfTE8Q8bwq-tXJZVvc-XXS1o2AGugCLcBGAsYHQ/s0/background2.png"></div>
-            <div class="rotateSlides"><img src="https://1.bp.blogspot.com/-HhKzDXo1kvQ/X52T80atZKI/AAAAAAAAGcI/l2aOarZPOpM9d_GxSqVc0unCxgpu01OJgCLcBGAsYHQ/s0/background3.png"></div>
+            <div class="rotateSlides"><img src="https://1.bp.blogspot.com/-QA4SsWxA5y0/YEuuMC8rWRI/AAAAAAAAG0Q/P6vJI_LqZ5cwbmWmPx89IleYxIsJwEyJQCLcBGAsYHQ/s0/background5-nova.png"></div>
             <div class="rotateSlides"><img src="https://1.bp.blogspot.com/-npVdZ1nW-Sw/X52V5KYQnMI/AAAAAAAAGcU/lSD9NaKb0VQzl15NVZYnH_d9GOiAhpoFQCLcBGAsYHQ/s0/background4.png"></div>
             <div class="rotateSlides"><img src="https://1.bp.blogspot.com/--FfdxsHzF-w/YEAXIJN2CUI/AAAAAAAAGvQ/PMWW-uwkYkEIC3wAG7fUCs16A-wSEqnGQCLcBGAsYHQ/s0/background5.png"></div>
         </div>
