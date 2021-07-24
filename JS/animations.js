99c5fa1ae7aad858c72abedbc430a96cfc1d5568
nauankiller15 <nauan.alcantara1@gmail.com>
2021-07-24 19:09:04 -0300

    ajuste no loading

--- JS/animations.js
+++ JS/animations.js
@@ -2,8 +2,10 @@
 // 
 // 
 // TELA DE CARREGAMENTO
-$(window).on("load", function() {
-    $(".loader-wrapper").fadeOut("slow");
+$(window).on('load', function() {
+    $('.loader-wrapper .spinner').fadeOut();
+    $('.loader-wrapper').delay(950).fadeOut('slow');
+    $('body').delay(950).css({ 'overflow': 'visible' });
 });
 // 
 // ---------------
